3ae13bcf9917cfcb55cac343cbf800f3268045b4
Rob Kennedy <brobzilla@gmai.com>
2023-05-09 22:45:48 -0400

    Changes for sensor training

--- TeamCode/src/test/java/org/cc4hrobotics/ftc/LearnJavaTest.java
+++ TeamCode/src/test/java/org/cc4hrobotics/ftc/LearnJavaTest.java
@@ -4,9 +4,14 @@ import static org.mockito.Mockito.mock;
 
 import android.content.Context;
 
+import com.qualcomm.robotcore.hardware.DigitalChannel;
+
 import org.cc4hrobotics.ftc.fakes.FakeHardwareMap;
+import org.cc4hrobotics.ftc.fakes.FakeTelemetry;
 import org.cc4hrobotics.ftc.fakes.drive.FakeExtendedDcMotor;
+import org.cc4hrobotics.ftc.fakes.sensors.FakeDigitalChannel;
 import org.cc4hrobotics.ftc.fakes.sensors.FakeDistanceSensor;
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -17,32 +22,53 @@ public class LearnJavaTest {
     private FakeExtendedDcMotor leftDriveMotor;
     private FakeExtendedDcMotor rightDriveMotor;
     private FakeDistanceSensor distanceSensor;
+    private FakeDistanceSensor distanceSensor2;
+    private FakeDigitalChannel digitalChannel;
+    private FakeTelemetry fakeTelemetry = new FakeTelemetry();
 
     @Test
     public void RunIt() {
         double distance = 10;
         LearnJavaOpMode ljt = new LearnJavaOpMode();
+
+        ljt.telemetry = fakeTelemetry;
         ljt.hardwareMap = hardwareMap;
         ljt.init();
 
+        digitalChannel.setState(true);
         sleep(5);
 
+        distanceSensor2.setDistance(100000);
         distanceSensor.setDistance(distance);
         for (int i = 0; i< 100; i++ ) {
+
+            fakeTelemetry.setLoopCount(i);
+            //Make adjustments to the gamepad and watch robot adjust
+            ljt.gamepad1.left_stick_x -= 0.03;
+            ljt.gamepad1.left_stick_y += 0.02;
+            ljt.gamepad1.a = !ljt.gamepad1.a;
+
+            if (leftDriveMotor.getPower() > 0 && rightDriveMotor.getPower() > 0)  {
+                distance = distance - 1;
+            } else if (leftDriveMotor.getPower() < 0 && rightDriveMotor.getPower() < 0)  {
+                distance = distance + 1;
+            }
+            distanceSensor.setDistance(Math.max(distance, 0.0));
+
+            distanceSensor2.setDistance(distanceSensor2.getDistance(DistanceUnit.CM)/2);
+
+            if (i == 25 && digitalChannel.getMode() == DigitalChannel.Mode.INPUT) {
+                digitalChannel.setState(false);
+                //override to zero for testing...
+                distanceSensor2.setDistance(3.14);
+            } else {
+                digitalChannel.setState(true);
+            }
+
+
+
             ljt.loop();
-
-                //Make adjustments to the gamepad and watch robot adjust
-                ljt.gamepad1.left_stick_x -= 0.03;
-                ljt.gamepad1.left_stick_y += 0.02;
-                ljt.gamepad1.a = !ljt.gamepad1.a;
-
-                if (leftDriveMotor.getPower() > 0 && rightDriveMotor.getPower() > 0)  {
-                    distance = distance - 1;
-                } else if (leftDriveMotor.getPower() < 0 && rightDriveMotor.getPower() < 0)  {
-                    distance = distance + 1;
-                }
-                distanceSensor.setDistance(distance);
-                sleep(3);
+            sleep(3);
         }
     }
 
@@ -60,6 +86,14 @@ public class LearnJavaTest {
 
         distanceSensor = new FakeDistanceSensor();
         hardwareMap.addDevice("distance_sensor", distanceSensor);
+
+        distanceSensor2 = new FakeDistanceSensor();
+        hardwareMap.addDevice("distance_sensor2", distanceSensor2);
+
+        digitalChannel = new FakeDigitalChannel();
+        hardwareMap.addDevice("sensor_digital", digitalChannel);
+
+
     }
 
     public void sleep(long seconds) {
