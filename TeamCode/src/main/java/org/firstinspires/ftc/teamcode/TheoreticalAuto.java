66e3c6dd6225016e3a18a95c0d8f07f2b24b7df3
cc4hftc <centrecounty4hFTC@gmail.com>
2024-01-06 11:52:42 -0500

    update auto

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TheoreticalAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TheoreticalAuto.java
@@ -6,22 +6,27 @@ import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.DistanceSensor;
 
 import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
 
 
-@Autonomous(name="DistanceSensor_ExtraDriving", group="eric")
-public class CopiedCode extends LinearOpMode {
+@Autonomous(name="Theoretical autonomous", group="eric")
+public class TheoreticalAuto extends LinearOpMode {
     DistanceSensor Ldistance;
     DistanceSensor Rdistance;
     private CRServo claw = null;
     private CRServo claw2 = null;
     private DcMotor lift = null;
+    public int Strt_to_center=38;
+    public int Rbot_width=18;
+    public int Center_to_tapeSIDE=11;
+    public int Strt_to_TAPEForwardOnly=46;
+    public int Colision_Tune_distance=2;
     @Override
     public void runOpMode() {
+
         SampleMecanumDrive drive = new SampleMecanumDrive(hardwareMap);
         Ldistance = hardwareMap.get(DistanceSensor.class, "Lsensor");
         Rdistance = hardwareMap.get(DistanceSensor.class, "Rsensor");
@@ -31,34 +36,36 @@ public class CopiedCode extends LinearOpMode {
         claw2.setDirection(CRServo.Direction.REVERSE);
         lift = hardwareMap.get(DcMotor.class, "lift");
 
-        Trajectory myTrajectory = drive.trajectoryBuilder(new Pose2d())
-                .forward(3.6)
+        Trajectory Drive_to_center = drive.trajectoryBuilder(new Pose2d())
+                .forward(Strt_to_center-Rbot_width)
                 .build();
-
+        Trajectory SideShiftLeft = drive.trajectoryBuilder(new Pose2d())
+                .strafeLeft(Center_to_tapeSIDE)
+                .build();
+        Trajectory SideShiftRight= drive.trajectoryBuilder( new Pose2d())
+                .strafeRight(Center_to_tapeSIDE)
+                .build();
+        Trajectory Go_To_Forward_tape=drive.trajectoryBuilder( new Pose2d())
+                .forward(Strt_to_TAPEForwardOnly-Strt_to_center)
+                .build();
         waitForStart();
 
         if (isStopRequested()) return;
-        Trajectory backwards = drive.trajectoryBuilder(new Pose2d())
-                        .back(1.0)
-                                .build();
-        drive.followTrajectory(myTrajectory);
+        Trajectory backwards_Tune = drive.trajectoryBuilder(new Pose2d())
+                .back(Colision_Tune_distance)
+                .build();
+        drive.followTrajectory(Drive_to_center);
         telemetry.addData("Rdistance: ",Rdistance.getDistance(DistanceUnit.CM));
         telemetry.update();
        if (Ldistance.getDistance(DistanceUnit.CM)<=30){
-            myTrajectory = drive.trajectoryBuilder(new Pose2d())
-                    .strafeLeft(2.0)
-                    .build();
+           drive.followTrajectory(SideShiftLeft);
+           drive.followTrajectory(backwards_Tune);
         } else if (Rdistance.getDistance(DistanceUnit.CM)<=30){
-            myTrajectory = drive.trajectoryBuilder( new Pose2d())
-                    .strafeRight(1.2)
-                    .build();
+           drive.followTrajectory(SideShiftRight);
+           drive.followTrajectory(backwards_Tune);
         } else{
-            myTrajectory = drive.trajectoryBuilder(new Pose2d())
-                    .forward(1.0)
-                    .build();
+            drive.followTrajectory(Go_To_Forward_tape);
         }
-        drive.followTrajectory(myTrajectory);
-        drive.followTrajectory(backwards);
         claw.setPower(1.0);
         claw2.setPower(1.0);
         sleep(3000);
