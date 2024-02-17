5c46e1d7c99a3a891604cac129bc3212a8e33831
cc4hftc <centrecounty4hFTC@gmail.com>
2024-02-17 14:01:17 -0500

    Fix some trajectories

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DucksTrajectories.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DucksTrajectories.java
@@ -12,9 +12,9 @@ public class DucksTrajectories {
 
     // Red Backstage
     public static final Pose2d redBackStartingPose = new Pose2d(12, -61, Math.toRadians(90));
-    public static final Pose2d redBackLeftLine = new Pose2d(13.5, -32, Math.toRadians(180));
+    public static final Pose2d redBackLeftLine = new Pose2d(13.5, -30, Math.toRadians(180));
     public static final Pose2d redBackCenterLine = new Pose2d(12, -36, Math.toRadians(90));
-    public static final Pose2d redBackRightLine = new Pose2d(22.5, -40, Math.toRadians(90));
+    public static final Pose2d redBackRightLine = new Pose2d(22.5, -42, Math.toRadians(90));
 
     // Blue Frontstage
     public static final Pose2d blueFrontStartingPose = new Pose2d(-36, 61, Math.toRadians(270));
@@ -30,17 +30,17 @@ public class DucksTrajectories {
 
     // Waypoints
     public static final Pose2d redStartingPoseReturnOffset = new Pose2d(0, 3, 0);
-    public static final Pose2d redBackStageWaypoint = new Pose2d(36, -58, Math.toRadians(90));
+    public static final Pose2d redBackStageWaypoint = new Pose2d(36, -58, Math.toRadians(0));
     public static final Pose2d blueStartingPoseReturnOffset = new Pose2d(0, -3, 0);
     public static final Pose2d blueBackStageWaypoint = new Pose2d(36, 58, Math.toRadians(270));
 
     // Backdrop
-    public static final Pose2d redBackdropLeft = new Pose2d(45, -30, Math.toRadians(0));;
-    public static final Pose2d redBackdropCenter = new Pose2d(45, -36, Math.toRadians(0));;
-    public static final Pose2d redBackdropRight = new Pose2d(45, -42, Math.toRadians(0));;
-    public static final Pose2d blueBackdropLeft = new Pose2d(45, 42, Math.toRadians(0));;
-    public static final Pose2d blueBackdropCenter = new Pose2d(45, 36, Math.toRadians(0));;
-    public static final Pose2d blueBackdropRight = new Pose2d(45, 30, Math.toRadians(0));;
+    public static final Pose2d redBackdropLeft = new Pose2d(40, -30, Math.toRadians(0));;
+    public static final Pose2d redBackdropCenter = new Pose2d(40, -36, Math.toRadians(0));;
+    public static final Pose2d redBackdropRight = new Pose2d(40, -42, Math.toRadians(0));;
+    public static final Pose2d blueBackdropLeft = new Pose2d(40, 42, Math.toRadians(0));;
+    public static final Pose2d blueBackdropCenter = new Pose2d(40, 36, Math.toRadians(0));;
+    public static final Pose2d blueBackdropRight = new Pose2d(4, 30, Math.toRadians(0));;
 
     // Parking
     public static final Pose2d redBackdropLeftParkWaypoint = new Pose2d(45, -14, Math.toRadians(180));
